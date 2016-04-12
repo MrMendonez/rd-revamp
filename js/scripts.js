3d791a3e9e80a28cfe750d714a9986c740d23bb2
MrMendonez <dmendonez@gmail.com>
2016-04-12 18:44:45 -0400

    init commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  // Slider
+  $('.slider').slider({full_width: true});
+
+  // Mobile Nav
+  $(".button-collapse").sideNav();
+
+  // Modal
+  $('.modal-trigger').leanModal();
+
+});
